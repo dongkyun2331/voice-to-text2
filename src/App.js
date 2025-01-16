0c9c1a46dab72d4b83e926a1832ee899702b34a2
dongkyun2331 <dongkyun2331@gmail.com>
2025-01-16 12:01:38 +0900

    줄바꿈 안하기

--- src/App.js
+++ src/App.js
@@ -47,9 +47,7 @@ const App = () => {
             interimTranscript += transcript;
           }
         }
-        setText((prevText) =>
-          (prevText.trim() + '\n' + finalTranscript.trim()).trim()
-        );
+        setText((prevText) => prevText + finalTranscript);
         setInterimText(interimTranscript);
 
         // 실시간으로 텍스트를 서버에 저장
